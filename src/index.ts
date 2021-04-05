3d3d7d908bf49db14c606916a7123cb614de8fa0
pratikg17 <pjgawand@gmail.com>
2021-04-05 12:50:32 +0530

    Router

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
 
 const app = express();
 app.use(json());
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
-  console.log('Listening on Port: 3000');
+  console.log('Listening on Port: 3000!!');
 });
