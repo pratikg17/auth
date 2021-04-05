a7f58853ff0f59adda2c38ea76a7896a07f88d20
pratikg17 <pjgawand@gmail.com>
2021-04-05 16:37:56 +0530

    Added User Model for Mongoose Doc

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Response, Request } from 'express';
 import { body, validationResult } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/User';
 
 const router = express.Router();
 
@@ -20,11 +21,21 @@ router.post(
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
 
-    // const { email, password } = req.body;
-    // res.send({});
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
+    const user = User.build({
+      email,
+      password,
+    });
+
+    await user.save();
+    res.status(201).send(user);
   }
 );
 
