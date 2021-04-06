f0298d6c33a079db388fb75c293c0fe14657eadf
pratikg17 <pjgawand@gmail.com>
2021-04-06 13:01:08 +0530

    First unit tets

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -3,10 +3,12 @@ import mongoose from 'mongoose';
 import { app } from '../app';
 
 let mongo: any;
-
 beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf';
+
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
+
   await mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -15,6 +17,7 @@ beforeAll(async () => {
 
 beforeEach(async () => {
   const collections = await mongoose.connection.db.collections();
+
   for (let collection of collections) {
     await collection.deleteMany({});
   }
