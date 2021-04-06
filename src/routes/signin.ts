0d6a18835af6c3854e1f5c1cd5af8f618fd54665
pratikg17 <pjgawand@gmail.com>
2021-04-06 11:29:01 +0530

    Sign IN

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/User';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
@@ -11,7 +15,32 @@ router.post(
     body('password').trim().notEmpty().withMessage('You must apply a password'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+    // Generate JWT
+    const userJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
